c22c19d85987d3d2373b332bcaa81475d5e0619b
iLem0n <peter.c.glade@googlemail.com>
2016-11-16 09:42:40 +0100

    Update filewatcher.py

--- filewatcher.py
+++ filewatcher.py
@@ -4,7 +4,7 @@ import time
 import sys
 import os
 import subprocess
-from daemon import Daemon 
+from daemon import DaemonContext
 
 from watchdog.observers import Observer
 from watchdog.events import PatternMatchingEventHandler
@@ -44,7 +44,7 @@ class EventHandler(PatternMatchingEventHandler):
         self.process(event)
 
 
-class Filewatcher(Daemon):
+class Filewatcher(DaemonContext):
     def run(self):
         args = sys.argv[1:]
         
