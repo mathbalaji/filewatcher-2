210b9b49dd672d871bce7664de7a5c1a2ccd0923
iLem0n <peter.c.glade@googlemail.com>
2016-11-16 09:07:55 +0100

    Update filewatcher.py

--- filewatcher.py
+++ filewatcher.py
@@ -27,14 +27,14 @@ class EventHandler(PatternMatchingEventHandler):
 
 	def process(self, event):
 		print event
-		"""
+		
 		event.event_type
 			'modified' | 'created' | 'moved' | 'deleted'
 		event.is_directory
 			True | False
 		event.src_path
 			path/to/file
-		"""
+		
 		self.do_handleChanges(event)
 	
 	def on_modified(self, event):
@@ -43,21 +43,73 @@ class EventHandler(PatternMatchingEventHandler):
 	def on_create(self, event):
 		self.process(event)
 
-with daemon
-if __name__ == '__main__':
-	args = sys.argv[1:]
-	logfile = "/var/log/filewatcher.log"
-	eventHandler = EventHandler()
-	eventHandler.logfile = open(logfile, 'a')
+class Filewatcher(Daemon):
+	def run(self):
+		args = sys.argv[1:]
+		
+		eventHandler = EventHandler()
+		eventHandler.logfile = open(logfile, 'a')
 
-	observer = Observer()
-	observer.schedule(eventHandler, path=args[0] if args else '.')
-	observer.start()
+		observer = Observer()
+		observer.schedule(eventHandler, path=args[0] if args else '.')
+		observer.start()
 
-	eventHandler.logfile.write("filewatcher.py is watching on: {0}".format(repr(os.getcwd())))
-	try:
+		eventHandler.logfile.write("filewatcher.py is watching on: {0}".format(repr(os.getcwd())))
+		try:
+			while True:
+				time.sleep(1)
+		except KeyboardInterrupt:
+			observer.stop()
+		observer.join()
+		
+		# Logging errors and exceptions
+		try:
+			pass
+		except Exception, e:
+			logging.exception('Human friendly error message, the exception will be captured and added to the log file automaticaly')
 		while True:
-			time.sleep(1)
-	except KeyboardInterrupt:
-		observer.stop()
-	observer.join()
+			time.sleep(60)
+			
+
+if __name__ == '__main__':
+	fw = FileWatcher(PIDFILE)
+	
+	if len(sys.argv) == 2:
+
+	if 'start' == sys.argv[1]:
+		try:
+			daemon.start()
+		except:
+			pass
+
+	elif 'stop' == sys.argv[1]:
+		print "Stopping ..."
+		daemon.stop()
+
+	elif 'restart' == sys.argv[1]:
+		print "Restaring ..."
+		daemon.restart()
+
+	elif 'status' == sys.argv[1]:
+		try:
+			pf = file(PIDFILE,'r')
+			pid = int(pf.read().strip())
+			pf.close()
+		except IOError:
+			pid = None
+		except SystemExit:
+			pid = None
+
+		if pid:
+			print 'YourDaemon is running as pid %s' % pid
+		else:
+			print 'YourDaemon is not running.'
+
+	else:
+		print "Unknown command"
+		sys.exit(2)
+		sys.exit(0)
+else:
+	print "usage: %s start|stop|restart|status" % sys.argv[0]
+	sys.exit(2)
+
